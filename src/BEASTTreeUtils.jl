00dff9153a345fc03bcdcd163789e166f6b52d8e
Gabe Hassler <gabriel.w.hassler@gmail.com>
2021-03-06 11:07:46 -0800

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.13

---
+++ src/BEASTTreeUtils.jl
@@ -0,0 +1,5 @@
+module BEASTTreeUtils
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using BEASTTreeUtils
+using Test
+
+@testset "BEASTTreeUtils.jl" begin
+    # Write your tests here.
+end
